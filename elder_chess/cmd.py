8d6a7e2cb6c9270e4b3b120c24bf0d577f9a8076
jq.yang@berkeley.edu <jq.yang@berkeley.edu>
2018-08-10 12:11:00 -0400

    save

--- elder_chess/cmd.py
+++ elder_chess/cmd.py
@@ -10,10 +10,10 @@ def mcts_player():
     policy_value_net = PolicyValueNet(model_file="models/best_policy.model")
     return MCTSPlayer(policy_value_net.policy_value, c_puct=5, n_playout=10000, is_selfplay=False)
 
-def mcts_player_at_branch(git_tag, n_playout, name='', tmp_repo_path='./tmp/AEC'):
-    build_dir = "build_" + git_tag
-    subprocess.check_call(['git', 'fetch', '--all', '--tags', '--prune'], cwd=tmp_repo_path)
-    subprocess.check_call(['git','checkout', 'tags/'+git_tag], cwd=tmp_repo_path)
+def mcts_player_at_branch(git_commit, n_playout, name='', tmp_repo_path='./tmp/AEC'):
+    build_dir = "build_" + git_commit
+    subprocess.check_call(['git', 'fetch', '--all'], cwd=tmp_repo_path)
+    subprocess.check_call(['git','checkout', git_commit], cwd=tmp_repo_path)
     subprocess.check_call(['git','submodule', 'update', '--init', '--recursive'], cwd=tmp_repo_path)
     print("Compiling native...")
     subprocess.check_call(['mkdir', '-p', os.path.join('native', build_dir)], cwd=tmp_repo_path)
@@ -21,15 +21,15 @@ def mcts_player_at_branch(git_tag, n_playout, name='', tmp_repo_path='./tmp/AEC'
     subprocess.check_call(['make', '-C',  os.path.join('native', build_dir)], cwd=tmp_repo_path)
     os.environ['LD_LIBRARY_PATH'] = os.path.join(tmp_repo_path, os.path.join('native', build_dir))
     import importlib.util
-    spec1 = importlib.util.spec_from_file_location(git_tag + "_module", os.path.join(tmp_repo_path, 'mcts_player.py'))
+    spec1 = importlib.util.spec_from_file_location(git_commit + "_module", os.path.join(tmp_repo_path, 'mcts_player.py'))
     player_module = importlib.util.module_from_spec(spec1)
     spec1.loader.exec_module(player_module)
 
-    spec2 = importlib.util.spec_from_file_location(git_tag + "_module", os.path.join(tmp_repo_path, 'tensorflow_policy.py'))
+    spec2 = importlib.util.spec_from_file_location(git_commit + "_module", os.path.join(tmp_repo_path, 'tensorflow_policy.py'))
     network_module = importlib.util.module_from_spec(spec2)
     spec2.loader.exec_module(network_module)
 
-    value_net = network_module.PolicyValueNet(model_file=os.path.join("models_"+git_tag, 'best_policy.model'))
+    value_net = network_module.PolicyValueNet(model_file=os.path.join("models_"+git_commit, 'best_policy.model'))
     return player_module.MCTSPlayer(value_net.policy_value, c_puct=5, n_playout=n_playout, is_selfplay=False, name=name)
 
 def player_argument(args):
